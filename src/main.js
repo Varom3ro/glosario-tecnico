d15e46a926cf3a163fde79b209e881d7ee9e2db3
varomero <admon.modacolors@gmail.com>
2026-05-19 08:45:07 -0400

    fix: use relative path for service worker registration in main.js

--- src/main.js
+++ src/main.js
@@ -1273,7 +1273,7 @@ function setupPWAInstall() {
 function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('./service-worker.js')
         .then((reg) => {
           console.log('Service Worker registrado con éxito. Scope:', reg.scope);
         })
